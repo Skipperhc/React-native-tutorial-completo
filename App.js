3ea7f3d12c121146a075d7a7fdccc211e310fa51
Vitor Hainosz <vrhc@gft.com>
2021-04-05 16:16:40 -0300

    Cheguei na parte do touchable, tem muita coisa para pesquisar sobre cada item que ele ensina, tem muitos diferentes tipos de abordagem para  sobre isso e pelo que eu entendi tem muitas maneiras de contornar o problema que estamos enfrentando, existe muitas abordagens para uma unica situação, para ficar bom no react acho que terei de decorar ou pelo menos entender as mais frequentes dessas

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { setStatusBarBackgroundColor, StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+
+import GoalItem from './componets/GoalItem';
+import GoalInput from './componets/GoalInput';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+  const goalInputHandler = (enteredGoal) => {
+    setEnteredGoal(enteredGoal)
+  }
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(
+      currentGoals =>
+        [
+          ...currentGoals,
+          { id: Math.random().toString(), value: goalTitle }
+        ]
+    );
+  }
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoalHandler} />
+      
+      <FlatList
+        data={courseGoals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={itemData =>
+          <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value}></GoalItem>
+        } />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
